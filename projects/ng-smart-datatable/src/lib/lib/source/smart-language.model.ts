c2fc89f31828451cf0aa7dd98a037bc067fbed2d
Ahmet ICAT <ahmeticat@gmail.com>
2020-01-26 19:57:52 +0300

    Set language properties to nullable

--- projects/ng-smart-datatable/src/lib/lib/source/smart-language.model.ts
+++ projects/ng-smart-datatable/src/lib/lib/source/smart-language.model.ts
@@ -1,10 +1,10 @@
 export class SmartLanguage {
-    actionsColumnHeader = 'Actions';
-    lengthMenu = 'Show **LENGTH** entries';
-    search: 'Search : **SEARCH**';
-    searchPlaceholder: 'Search';
-    info: 'Showing **START** to **END** of **TOTAL** entries';
-    filteredInfo: '(Filtered from **TOTAL** entries)';
-    paginationPrevious: 'Previous';
-    paginationNext: 'Next';
+    actionsColumnHeader?: string = 'Actions';
+    lengthMenu?: string = 'Show **LENGTH** entries';
+    search?: 'Search : **SEARCH**';
+    searchPlaceholder?: 'Search';
+    info?: 'Showing **START** to **END** of **TOTAL** entries';
+    filteredInfo?: '(Filtered from **TOTAL** entries)';
+    paginationPrevious?: 'Previous';
+    paginationNext?: 'Next';
 }
